511c2fd0c171f01a4e1e06feb3d1cbaf77985a62
Nafeu Nasir <nafeu.nasir@gmail.com>
2017-06-01 01:12:35 -0400

    Add proper note keydown display logic

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -9,8 +9,30 @@ $(document).ready(function(){
   var contentChord = $("#content-chord");
 
   socket.on("update", function(data){
-    contentNote.text(data.note);
+    contentNote.empty();
+    data.note.forEach(function(item){
+      contentNote.append(createNoteElement(item));
+    });
     contentChord.text(data.chord);
   });
 
 });
+
+function createNoteElement(noteArray) {
+  var out = $("<div>", { class: "note-block" }),
+      noteMain = $("<div>", { class: "note-main" }),
+      noteAlt = $("<div>", { class: "note-alt" });
+
+  noteMain.text(noteArray[0]);
+  out.append(noteMain);
+
+  if (noteArray.length > 1) {
+    noteAlt.text(noteArray[1]);
+  } else {
+    noteAlt.text("...");
+  }
+
+  out.append(noteAlt);
+
+  return out;
+}
